c790533f96ac8f35bd16444a55e84fd0de2dda71
blessing21-hash <blessingkusiwani@gmail.com>
2025-07-01 15:33:46 +0200

    updated at last

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Routes,Route } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Resume from './pages/Resume.jsx';
 import Contact from './pages/Contact.jsx';
 import './App.css';
-// updates
 
 const App = () => {
   return (
-    // <Router>
-    //   <Header />
-    //   <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -22,9 +16,6 @@ const App = () => {
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      /*{ </main>
-      <Footer />
-    </Router> }*/
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import './Home.css';
-// import profilePic from '../assets/blessing.jpg'; // Replace with your actual image path
-// import Header from '../Components/Header';
-// import Footer from '../Components/Footer';
-
-
-// const Home = () => {
-//   return (
-   
-//     <section className="home-section">
-//         <Header/>
-//       <div className="home-content">
-//         <img src={profilePic} alt="Blessing Kusiwani" className="profile-img" />
-//         <div className="intro-text">
-//           <h1>Hi, I'm <span>Blessing Kusiwani</span></h1>
-//           <p className="tagline">Creative Frontend Developer crafting modern & responsive web experiences.</p>
-//           <a href="/projects">
-//             <button className="cta-button">View My Work</button>
-//           </a>
-//         </div>
-//       </div>
-//       <Footer/>
-//     </section>
-    
-//   );
-// };
-
-// export default Home;
-
-
-
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Components/Header';
@@ -44,7 +11,6 @@ const Home = () => {
     <div className="home">
       <Header />
       <Home2/>
-
       <Footer />
     </div>
   );
